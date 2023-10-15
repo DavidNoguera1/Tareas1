66977b15e377f532fe3b9275a740c352b888f34c
DESKTOP-LFOCKGF\David Noguera <davidnoguera0912@gmail.com>
2023-10-14 22:36:32 -0500

    Iniciando un Nodo

---
+++ tareas/src/main/java/umariana/tareas/Tareas.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package umariana.tareas;
+
+import java.util.Date;
+
+/**
+ *
+ * @author David Noguera Definimos los atributos que se le asignaran a cada
+ * tarea
+ */
+public class Tareas {
+
+    private int id;
+    private String titulo;
+    private String descripcion;
+    private Date fechaV;
+
+    public Tareas() {
+    }
+
+    public Tareas(int id, String titulo, String descripcion, Date fechaV) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.fechaV = fechaV;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFechaV() {
+        return fechaV;
+    }
+
+    public void setFechaV(Date fechaV) {
+        this.fechaV = fechaV;
+    }
+
+}
